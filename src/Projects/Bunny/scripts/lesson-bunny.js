7dcc099de5ae2a2bebb8471ed21f59dfa281c7bc
Jermbo <jermbo@cosmicstrawberry.com>
2019-09-01 13:22:18 -0400

    adding head eyes and mouth portion of lesson

--- src/Projects/Bunny/scripts/lesson-bunny.js
+++ src/Projects/Bunny/scripts/lesson-bunny.js
@@ -118,6 +118,108 @@ const ears = {
   ],
 };
 
-const steps = [wrapper, ears];
+const head = {
+  desc: "Now that we have our bunny container, let's start at the top with the ears.",
+  example: `
+&lt;div class=&quot;wrapper&quot;&gt;
+  &lt;div class=&quot;bunny&quot;&gt;
+    &lt;div class=&quot;ears&quot;&gt;
+      &lt;div class=&quot;ear-left&quot;&gt;&lt;/div&gt;
+      &lt;div class=&quot;ear-right&quot;&gt;&lt;/div&gt;
+    &lt;/div&gt;
+    &lt;div class=&quot;head&quot;&gt;
+      &lt;div class=&quot;eyes&quot;&gt;
+        &lt;div class=&quot;eye-left&quot;&gt;&lt;/div&gt;
+        &lt;div class=&quot;eye-right&quot;&gt;&lt;/div&gt;
+      &lt;/div&gt;
+      &lt;div class=&quot;mouth&quot;&gt;&lt;/div&gt;
+    &lt;/div&gt;
+    &lt;div class=&quot;body&quot;&gt;
+      &lt;div class=&quot;arms&quot;&gt;
+        &lt;div class=&quot;arm-left&quot;&gt;&lt;/div&gt;
+        &lt;div class=&quot;arm-right&quot;&gt;&lt;/div&gt;
+      &lt;/div&gt;
+      &lt;div class=&quot;feet&quot;&gt;
+        &lt;div class=&quot;foot-left&quot;&gt;&lt;/div&gt;
+        &lt;div class=&quot;foot-right&quot;&gt;&lt;/div&gt;
+      &lt;/div&gt;
+    &lt;/div&gt;
+  &lt;/div&gt;
+&lt;/div&gt;`,
+  tasks: [
+    {
+      type: "children",
+      el: ".bunny",
+      test: { item: "div", value: 2 },
+      instructions: "Create a &lt;div&gt;&lt;/div&gt; inside of bunny",
+      hint: "Add &lt;div&gt;&lt;/div&gt; inside of bunny",
+    },
+    {
+      type: "attribute",
+      el: ".head",
+      test: { item: "className", value: "head" },
+      instructions: "Give that &lt;div&gt;&lt;/div&gt; a class of 'head'",
+      hint: "Add class=&quot;head&quot; to the &lt;div&gt;",
+    },
+    {
+      type: "children",
+      el: ".head",
+      test: { item: "div", value: 1 },
+      instructions: "Create a &lt;div&gt;&lt;/div&gt; inside of head",
+      hint: "Add &lt;div&gt;&lt;/div&gt; inside of head",
+    },
+    {
+      type: "attribute",
+      el: ".eyes",
+      test: { item: "className", value: "eyes" },
+      instructions: "Give that &lt;div&gt;&lt;/div&gt; a class of 'eyes'",
+      hint: "Add class=&quot;eyes&quot; to the &lt;div&gt;",
+    },
+    {
+      type: "children",
+      el: ".eyes",
+      test: { item: "div", value: 1 },
+      instructions: "Create a &lt;div&gt;&lt;/div&gt; inside of eyes",
+      hint: "Add &lt;div&gt;&lt;/div&gt; inside of eyes",
+    },
+    {
+      type: "attribute",
+      el: ".eye-left",
+      test: { item: "className", value: "eye-left" },
+      instructions: "Give that &lt;div&gt;&lt;/div&gt; a class of 'eye-left'",
+      hint: "Add class=&quot;eye-left&quot; to the &lt;div&gt;",
+    },
+    {
+      type: "children",
+      el: ".eyes",
+      test: { item: "div", value: 2 },
+      instructions: "Create a &lt;div&gt;&lt;/div&gt; inside of eyes",
+      hint: "Add &lt;div&gt;&lt;/div&gt; inside of eyes",
+    },
+    {
+      type: "attribute",
+      el: ".eye-right",
+      test: { item: "className", value: "eye-right" },
+      instructions: "Give that &lt;div&gt;&lt;/div&gt; a class of 'eye-right'",
+      hint: "Add class=&quot;eye-right&quot; to the &lt;div&gt;",
+    },
+    {
+      type: "children",
+      el: ".head",
+      test: { item: "div", value: 2 },
+      instructions: "Create another &lt;div&gt;&lt;/div&gt; inside of head",
+      hint: "Add &lt;div&gt;&lt;/div&gt; inside of head",
+    },
+    {
+      type: "attribute",
+      el: ".mouth",
+      test: { item: "className", value: "mouth" },
+      instructions: "Give that &lt;div&gt;&lt;/div&gt; a class of 'mouth'",
+      hint: "Add class=&quot;mouth&quot; to the &lt;div&gt;",
+    },
+  ],
+};
+
+const steps = [wrapper, ears, head];
 // Pass the rules into this function and let the magic happen in the object
 window.load = Checker.loadTests(steps);
