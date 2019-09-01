402aa9a152280362e18e206a1719522b37c6729f
Jermbo <jermbo@cosmicstrawberry.com>
2019-09-01 18:30:57 -0400

    getting ears lesson started

--- src/Projects/Owl/scripts/lesson-owl.js
+++ src/Projects/Owl/scripts/lesson-owl.js
@@ -46,6 +46,67 @@ const wrapper = {
   ],
 };
 
-const steps = [wrapper];
+const ears = {
+  desc: "Knocking out the ears would be a good place to start.",
+  example: `&lt;div class=&quot;wrapper&quot;&gt;
+  &lt;div class=&quot;owl&quot;&gt;
+    &lt;div class=&quot;ear-left&quot;&gt;&lt;/div&gt;
+    &lt;div class=&quot;ear-right&quot;&gt;&lt;/div&gt;
+    &lt;div class=&quot;eyes&quot;&gt;
+      &lt;div class=&quot;eye eye-left&quot;&gt;
+        &lt;div class=&quot;eye-inner&quot;&gt;&lt;/div&gt;
+      &lt;/div&gt;
+      &lt;div class=&quot;eye eye-right&quot;&gt;
+        &lt;div class=&quot;eye-inner&quot;&gt;&lt;/div&gt;
+      &lt;/div&gt;
+    &lt;/div&gt;
+    &lt;div class=&quot;beak&quot;&gt;&lt;/div&gt;
+    &lt;div class=&quot;body&quot;&gt;
+      &lt;div class=&quot;dot-1&quot;&gt;&lt;/div&gt;
+      &lt;div class=&quot;dot-2&quot;&gt;&lt;/div&gt;
+      &lt;div class=&quot;dot-3&quot;&gt;&lt;/div&gt;
+      &lt;div class=&quot;dot-4&quot;&gt;&lt;/div&gt;
+      &lt;div class=&quot;dot-5&quot;&gt;&lt;/div&gt;
+    &lt;/div&gt;
+    &lt;div class=&quot;feet&quot;&gt;
+      &lt;div class=&quot;foot-left&quot;&gt;&lt;/div&gt;
+      &lt;div class=&quot;foot-right&quot;&gt;&lt;/div&gt;
+    &lt;/div&gt;
+    &lt;div class=&quot;shadow&quot;&gt;&lt;/div&gt;
+  &lt;/div&gt;
+&lt;/div&gt;`,
+  tasks: [
+    {
+      type: "children",
+      el: ".owl",
+      test: { item: "div", value: 1 },
+      instructions: "Create a &lt;div&gt;&lt;/div&gt; inside owl",
+      hint: "Add &lt;div&gt;&lt;/div&gt;",
+    },
+    {
+      type: "attribute",
+      el: ".ear-left",
+      test: { item: "className", value: "ear-left" },
+      instructions: "Give that &lt;div&gt;&lt;/div&gt; a class of 'ear-left'",
+      hint: "Add class=&quot;ear-left&quot; to the &lt;div&gt;",
+    },
+    {
+      type: "children",
+      el: ".owl",
+      test: { item: "div", value: 2 },
+      instructions: "Create a &lt;div&gt;&lt;/div&gt; inside owl",
+      hint: "Add &lt;div&gt;&lt;/div&gt;",
+    },
+    {
+      type: "attribute",
+      el: ".ear-right",
+      test: { item: "className", value: "ear-right" },
+      instructions: "Give that &lt;div&gt;&lt;/div&gt; a class of 'ear-right'",
+      hint: "Add class=&quot;ear-right&quot; to the &lt;div&gt;",
+    },
+  ],
+};
+
+const steps = [wrapper, ears];
 // Pass the rules into this function and let the magic happen in the object
 window.load = Checker.loadTests(steps);
