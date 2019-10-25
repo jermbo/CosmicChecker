4349392c0e6625c8fb6a090823cb6243f6ce57fe
jermbo <jermbo@cosmicstrawberry.com>
2019-10-25 10:40:04 -0400

    updating images

--- gulp/images.js
+++ gulp/images.js
@@ -1,60 +1,31 @@
 const gulp = require("gulp");
 const $ = require("gulp-load-plugins")({ lazy: true });
-const resizer = require("@zellwk/resize-images");
 
-const { images, tmpPath } = require("./_config");
+const { images } = require("./_config");
 
 const src = images.source;
 const build = images.build;
-const imageSizes = images.options.sizes;
 
-const tmpOutput = `${tmpPath}/minified`;
-const tmpOutput2 = `${tmpPath}/resized`;
 
-function jpegToWebp() {
-  return gulp
-    .src(`${src}.{jpeg,jpg}`)
-    .pipe($.newer({ dest: tmpOutput, ext: ".webp" }))
-    .pipe($.webp({ quality: 80 }))
-    .pipe(gulp.dest(tmpOutput));
-}
-
-function pngToWebp() {
-  return gulp
-    .src(`${src}.png`)
-    .pipe($.newer({ dest: tmpOutput, ext: ".webp" }))
-    .pipe($.webp({ lossless: true }))
-    .pipe(gulp.dest(tmpOutput));
-}
-
-function minifyImages() {
-  return gulp
-    .src(`${src}.{png,jpg,jpeg,gif}`)
-    .pipe($.newer(tmpOutput))
-    .pipe($.imagemin([$.imagemin.jpegtran({ progressive: true }), $.imagemin.optipng({ optimizationLevel: 5 })]))
-    .pipe(gulp.dest(tmpOutput));
-}
-
-function resizeImages() {
-  return resizer({
-    inputDir: tmpOutput,
-    outputDir: tmpOutput2,
-    outputSizes: imageSizes,
-  });
+function copyPSDsToDist() {
+  console.log(`${src}.psd`);
+  return gulp.src(`${src}.psd`).pipe(gulp.dest(build));
 }
 
 function copyImagesToDist() {
-  return gulp.src(`${tmpOutput2}/**/*`).pipe(gulp.dest(build));
+  console.log(`${src}.{jpg,JPG,jpeg,JPEG,png,PNG,gif,GIF,svg}`);
+  return gulp.src(`${src}.{jpg,JPG,jpeg,JPEG,png,PNG,gif,GIF,svg}`).pipe(gulp.dest(build));
 }
 
 function copySvgToDist() {
-  return gulp.src(`${src}/**/*.svg`).pipe(gulp.dest(build));
+  console.log(`${src}.svg`);
+  return gulp.src(`${src}.svg`).pipe(gulp.dest(build));
 }
 
 const compileImages = gulp.series(
-  gulp.parallel(jpegToWebp, pngToWebp, minifyImages),
-  gulp.parallel(resizeImages),
-  gulp.parallel(copyImagesToDist, copySvgToDist),
+  // gulp.parallel(jpegToWebp, pngToWebp, minifyImages),
+  // gulp.parallel(resizeImages),
+  gulp.parallel(copyImagesToDist, copySvgToDist, copyPSDsToDist),
 );
 
 exports.src = src;
