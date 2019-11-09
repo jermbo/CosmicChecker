e23b4c110669adbeffb249358cd1ea719206e010
Jermbo <jermbo@cosmicstrawberry.com>
2019-11-09 08:50:34 -0500

    adding check for megaman images

--- src/Projects/BeginnerCSS/Megaman/scripts/megaman.js
+++ src/Projects/BeginnerCSS/Megaman/scripts/megaman.js
@@ -1,59 +1,112 @@
-const heading = {
-  desc: "One of first things to style are fonts. Let's learn about some ways we can make fonts look really good.",
-  example: `h1 {
-  color: #1c77c3;
-  background: #7fc6ff;
-  font-family: Helvetica;
-  text-align: center;
-  margin: 0px;
-  padding: 20px;
+const row1 = {
+  desc: "Let's add the images for each character",
+  example: `.bubbleman {
+  background-image: url(../images/bubbleman.jpg);
+}
+.airman {
+  background-image: url(../images/airman.jpg);
+}
+.quickman {
+  background-image: url(../images/quickman.jpg);
 }`,
   tasks: [
     {
       type: "externalStyles",
-      el: "h1",
-      test: { item: "color", value: "#1c77c3" },
-      instructions: "Give h1 'color' of #1c77c3",
-      hint: "color: #1c77c3;",
+      el: ".bubbleman",
+      test: { item: "backgroundImage", value: "url(\"../images/bubbleman.jpg\")" },
+      instructions: "Give h1 'background-image' of url(\"../images/bubbleman.jpg\")",
+      hint: "background-image: url(\"../images/bubbleman.jpg\")",
     },
     {
       type: "externalStyles",
-      el: "h1",
-      test: { item: "fontFamily", value: "Helvetica" },
-      instructions: "Change default 'font-family' to Helvetica",
-      hint: "font-family: Helvetica;",
+      el: ".airman",
+      test: { item: "backgroundImage", value: "url(\"../images/airman.jpg\")" },
+      instructions: "Give h1 'background-image' of url(\"../images/airman.jpg\")",
+      hint: "background-image: url(\"../images/airman.jpg\")",
     },
     {
       type: "externalStyles",
-      el: "h1",
-      test: { item: "textAlign", value: "center" },
-      instructions: "Center text with 'text-align'",
-      hint: "text-align: center;",
-    },
-    {
-      type: "externalStyles",
-      el: "h1",
-      test: { item: "margin", value: "0px" },
-      instructions: "Remove extra margin. Set 'margin' to 0px",
-      hint: "margin: 0px;",
-    },
-    {
-      type: "externalStyles",
-      el: "h1",
-      test: { item: "padding", value: "20px" },
-      instructions: "Set 'padding' to 20px",
-      hint: "padding: 20px;",
-    },
-    {
-      type: "externalStyles",
-      el: "h1",
-      test: { item: "background", value: "#7fc6ff" },
-      instructions: "Set 'background' to #7fc6ff;",
-      hint: "background: #7fc6ff;",
+      el: ".quickman",
+      test: { item: "backgroundImage", value: "url(\"../images/quickman.jpg\")" },
+      instructions: "Give h1 'background-image' of url(\"../images/quickman.jpg\")",
+      hint: "background-image: url(\"../images/quickman.jpg\")",
     },
   ],
 };
 
-const steps = [heading];
+const row2 = {
+  desc: "Let's add the images for each character",
+  example: `.heatman {
+  background-image: url(../images/heatman.jpg);
+}
+.drwily {
+  background-image: url(../images/drwily.jpg);
+}
+.woodman {
+  background-image: url(../images/woodman.jpg);
+}`,
+  tasks: [
+    {
+      type: "externalStyles",
+      el: ".heatman",
+      test: { item: "backgroundImage", value: "url(\"../images/heatman.jpg\")" },
+      instructions: "Give h1 'background-image' of url(\"../images/heatman.jpg\")",
+      hint: "background-image: url(\"../images/heatman.jpg\")",
+    },
+    {
+      type: "externalStyles",
+      el: ".drwily",
+      test: { item: "backgroundImage", value: "url(\"../images/drwily.jpg\")" },
+      instructions: "Give h1 'background-image' of url(\"../images/drwily.jpg\")",
+      hint: "background-image: url(\"../images/drwily.jpg\")",
+    },
+    {
+      type: "externalStyles",
+      el: ".woodman",
+      test: { item: "backgroundImage", value: "url(\"../images/woodman.jpg\")" },
+      instructions: "Give h1 'background-image' of url(\"../images/woodman.jpg\")",
+      hint: "background-image: url(\"../images/woodman.jpg\")",
+    },
+  ],
+};
+
+const row3 = {
+  desc: "Let's add the images for each character",
+  example: `
+.metalman {
+  background-image: url(../images/metalman.jpg);
+}
+.flashman {
+  background-image: url(../images/flashman.jpg);
+}
+.crashman {
+  background-image: url(../images/crashman.jpg);
+}`,
+  tasks: [
+    {
+      type: "externalStyles",
+      el: ".metalman",
+      test: { item: "backgroundImage", value: "url(\"../images/metalman.jpg\")" },
+      instructions: "Give h1 'background-image' of url(\"../images/metalman.jpg\")",
+      hint: "background-image: url(\"../images/metalman.jpg\")",
+    },
+    {
+      type: "externalStyles",
+      el: ".flashman",
+      test: { item: "backgroundImage", value: "url(\"../images/flashman.jpg\")" },
+      instructions: "Give h1 'background-image' of url(\"../images/flashman.jpg\")",
+      hint: "background-image: url(\"../images/flashman.jpg\")",
+    },
+    {
+      type: "externalStyles",
+      el: ".crashman",
+      test: { item: "backgroundImage", value: "url(\"../images/crashman.jpg\")" },
+      instructions: "Give h1 'background-image' of url(\"../images/crashman.jpg\")",
+      hint: "background-image: url(\"../images/crashman.jpg\")",
+    },
+  ],
+};
+
+const steps = [row1, row2, row3];
 // Pass the rules into this function and let the magic happen
 window.load = Checker.loadTests(steps);
