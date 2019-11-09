4c6ba4e8e9866337426552a262f30dc208fc43f1
Jermbo <jermbo@cosmicstrawberry.com>
2019-11-09 09:35:02 -0500

    updating megaman and zips

--- src/Projects/BeginnerCSS/Megaman/scripts/main.js
+++ src/Projects/BeginnerCSS/Megaman/scripts/main.js
@@ -1,23 +1,23 @@
-let characters;
+let megamanCharacters;
 let selectedTextDisplay;
 
 function init() {
-  characters = [...document.querySelectorAll(".character")];
+  megamanCharacters = [...document.querySelectorAll(".character")];
   selectedTextDisplay = document.querySelector(".selected-display");
 
-  if (characters.length == 0 || !selectedTextDisplay) {
-    console.error("There are no characters or text to interact with. Try building the HTML out a bit more.");
+  if (megamanCharacters.length == 0 || !selectedTextDisplay) {
+    console.error("There are no megamanCharacters or text to interact with. Try building the HTML out a bit more.");
     return;
   }
 
-  characters.forEach(character => {
+  megamanCharacters.forEach(character => {
     character.addEventListener("click", selectCharacter);
   });
 }
 
 function selectCharacter(e) {
   e.preventDefault();
-  characters.forEach(c => c.classList.remove("selected"));
+  megamanCharacters.forEach(c => c.classList.remove("selected"));
   e.target.classList.add("selected");
   selectedTextDisplay.innerText = e.target.innerText;
 }
